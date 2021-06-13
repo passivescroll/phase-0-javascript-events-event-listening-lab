0a1a639745b013ed48b70ad489e338cf2064bd96
sky <flahertysky12@gmail.com>
2021-06-13 14:41:32 -0400

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+
+const input = document.getElementById('input');
+
+const main = document.getElementById('main');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+
+main.addEventListener('click', clickAlert);
+
+
